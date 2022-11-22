ec9ff812cf2af1b9776a8021d9761ed56a45a52d
Heisenberg <buffer_finance@protonmail.com>
2022-11-22 15:25:42 +0530

    fixed #023

--- tests/test_binary_options.py
+++ tests/test_binary_options.py
@@ -1528,9 +1528,7 @@ class BinaryOptionTesting(object):
             == final_pool_tokenX_balance - initial_pool_tokenX_balance
             == 0
         ), "Wrong transfer of funds"
-        with brownie.reverts(
-            "ERC721: owner query for nonexistent token"
-        ):  # Option burnt
+        with brownie.reverts("ERC721: invalid token ID"):  # Option burnt
             self.tokenX_options.ownerOf(1)
 
         txn = self.unlock_options([(0, 390e8)])
@@ -1554,9 +1552,7 @@ class BinaryOptionTesting(object):
             == final_pool_tokenX_balance - initial_pool_tokenX_balance
             == 0
         ), "Wrong transfer of funds"
-        with brownie.reverts(
-            "ERC721: owner query for nonexistent token"
-        ):  # Option burnt
+        with brownie.reverts("ERC721: invalid token ID"):  # Option burnt
             self.tokenX_options.ownerOf(1)
 
         txn = self.unlock_options([(0, 390e8)])
